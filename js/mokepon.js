5f8880dce9f2e59266583b3f08e688a4bcdee742
Hernando Barrera <diego.barrera-b@uniminuto.edu.co>
2025-12-07 00:53:14 -0500

    se implementa logica para batalla en el juego mokepon

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,45 +1,72 @@
-
+let spanAtaqueJugador = document.getElementById('ataque-jugador')
+let spanAtaqueComputadora = document.getElementById('ataque-computadora')
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota')
+    let atacarFuego = document.getElementById('boton-fuego')
+    let atacarAgua = document.getElementById('boton-agua')
+    let atacarTierra = document.getElementById('boton-tierra')
+    let ataques = ['fuego', 'agua', 'tierra']
+
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 
-}
+    atacarFuego.addEventListener('click', ()=>
+    {spanAtaqueJugador = 'fuego';
+    spanAtaqueComputadora = ataques[(Math.floor(Math.random()*ataques.length))]
+    batalla()
+    crearMensaje()
+    })
 
+    atacarAgua.addEventListener('click', ()=>
+    {spanAtaqueJugador = 'agua';
+    spanAtaqueComputadora = ataques[(Math.floor(Math.random()*ataques.length))]
+    batalla()
+    crearMensaje()
+    })
+
+    atacarTierra.addEventListener('click', ()=>
+    {spanAtaqueJugador = 'tierra';
+    spanAtaqueComputadora = ataques[(Math.floor(Math.random()*ataques.length))]
+    batalla()
+    crearMensaje()
+
+    })
+}
 function seleccionarMascotaJugador(){
-    
     let spanMascotaJugador = document.getElementById('mascota-jugador');
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
     let mascotas = [ "hipodoge", "capipepo", "ratigueya", "lagostelvis", "tucapalma", "pydos"]
-
     if (document.getElementById('hipodoge').checked){
-    //alert('mascota seleccionada hipodogue')
     spanMascotaJugador.innerHTML = 'hipodoge'
     }
     else if (document.getElementById('capipepo').checked){
-    //alert('mascota seleccionada capipepo')
     spanMascotaJugador.innerHTML = 'capipepo'
     }
     else if  (document.getElementById('ratigueya').checked){
-    //alert('mascota seleccionada ratigueya')
     spanMascotaJugador.innerHTML = 'ratigueya'
     }
-    /*
-    else if  (document.getElementById('lagostelvis').checked){
-    //alert('mascota seleccionada lagostelvis')
-    spanMascotaJugador.innerHTML ='lagostelvis'    
-    }
-    else if  (document.getElementById('pydos').checked){
-    //alert('mascota seleccionada pydos')
-    spanMascotaJugador.innerHTML= 'pydos'
-    }
-    */
     else {
       alert("debesa seleccionar mascota")
     }
     spanMascotaEnemigo.innerHTML = mascotas[(Math.floor(Math.random()*5))+1]
 }
 
+function crearMensaje (){
+    let sectionMensaje=document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML='tu mascota ataco con '+ spanAtaqueJugador+' y la computadora ataco con'+spanAtaqueComputadora 
+    sectionMensaje.appendChild(parrafo)
+}
 
-
+function batalla(){
+if (spanAtaqueJugador == spanAtaqueComputadora){
+    alert ("usted digito " + spanAtaqueJugador + " y la computadora " + spanAtaqueComputadora + " por tanto es un empate")  
+}
+    else if ( spanAtaqueJugador == 'agua' && spanAtaqueComputadora == 'fuego' || spanAtaqueJugador == 'fuego' && spanAtaqueComputadora == 'tierra'  || spanAtaqueJugador == 'tierra' && spanAtaqueComputadora == 'agua'){
+        alert("usted selecciono " + spanAtaqueJugador + " y la computadora " + spanAtaqueComputadora + " por tanto usted GANO")
+    }
+        else{
+            alert("usted selecciono " + spanAtaqueJugador + " y la computadora " + spanAtaqueComputadora + " por tanto usted PERDIO")
+        }
+}
 window.addEventListener('load', iniciarJuego)
 
